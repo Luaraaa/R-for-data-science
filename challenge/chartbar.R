f97577a2cf078072a9063eca1fea3d3e450462cc
kimberlyhue <elleberly829@gmail.com>
2024-03-05 22:06:56 -0300

    mudei a largura de 200 para 0.5

--- challenge/chartbar.R
+++ challenge/chartbar.R
@@ -5,4 +5,4 @@ library(ggthemes)
 
 diamonds$carat <- cut(diamonds$carat, breaks = 10)
 ggplot(diamonds, aes(x = carat, fill = carat_cat)) +
-    geom_histogram(binwidth = 20)
+    geom_histogram(binwidth = 0.5)
