65852c6197074be7b3a257dc2024d7f766374eee
kimberlyhue <elleberly829@gmail.com>
2024-03-06 18:14:11 -0300

    deixando organizado e salvando o script chartbar.R

--- challenge/chartbar.R
+++ challenge/chartbar.R
@@ -3,6 +3,6 @@ library(palmerpenguins)
 library(dplyr)
 library(ggthemes)
 
-diamonds$carat <- cut(diamonds$carat, breaks = 10)
+diamonds$carat_cat <- cut(diamonds$carat, breaks = 10)
 ggplot(diamonds, aes(x = carat, fill = carat_cat)) +
     geom_histogram(binwidth = 0.5)
