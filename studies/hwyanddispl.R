8072e15624122c6f099c7140065559a0644bf511
kimberlyhue <elleberly829@gmail.com>
2024-03-08 02:07:38 -0300

    criando um gráfico de dispersão que relaciona bill_depth_mm e bill_length_mm salvando as modificações de hwyanddispl.R

--- studies/hwyanddispl.R
+++ studies/hwyanddispl.R
@@ -8,7 +8,7 @@ mpg$cyl <- as.factor(mpg$cyl)
 str(mpg$cyl)
 
 ggplot(mpg, aes(x = hwy, y = displ)) +
-  geom_point(aes(shape = cyl), size = 4) +
+  geom_point(aes(shape = cyl), size = 4, stroke = 0.75) +
   labs(
     title = "Relação entre economia de combustível e deslocamento do motor",
     subtitle = "Incluindo ano e número de cilindros",
