0baa1d72d8d627d3bb9d9b5f2b5e4918ebf07aef
kimberlyhue <elleberly829@gmail.com>
2024-03-08 01:41:41 -0300

    usando a função stroke()

--- studies/hwyanddispl.R
+++ studies/hwyanddispl.R
@@ -1,11 +1,18 @@
 library(ggplot2)
 library(dplyr)
-library(viridis)
+library(RColorBrewer)
+library(ggthemes)
 
 # conversão da variável cyl para fator (variável categórica)
 mpg$cyl <- as.factor(mpg$cyl)
 str(mpg$cyl)
 
 ggplot(mpg, aes(x = hwy, y = displ)) +
-    geom_point(aes(color = year, size = year, shape = cyl)) +
-    scale_color_viridis()
+  geom_point(aes(shape = cyl), size = 4) +
+  labs(
+    title = "Relação entre economia de combustível e deslocamento do motor",
+    subtitle = "Incluindo ano e número de cilindros",
+    x = "Combustivel por milhas em galão",
+    y = "Deslocamento do motor (litros)"
+  ) +
+    theme_clean()
